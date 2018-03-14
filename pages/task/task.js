e67e87688b3fc854b3489e8c1d1b3caf03b0dad5
kaiwen1811 <178158428@qq.com>
2018-03-14 22:53:54 +0800

    add task

--- pages/task/task.js
+++ pages/task/task.js
@@ -8,6 +8,14 @@ Page({
   
   },
 
+  //自定义函数
+  formSubmit: function (e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+  },
+  formReset: function () {
+    console.log('form发生了reset事件')
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
